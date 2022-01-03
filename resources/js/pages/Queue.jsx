31ab7665b63a0eb73df3da575ad4aaa25620fede
Javier García <javier.garcia@decowood.es>
2022-01-03 19:26:55 +0100

    Preparing queue route

---
+++ resources/js/pages/Queue.jsx
@@ -0,0 +1,10 @@
+
+function Queue() {
+    return (
+        <div className="alert alert-warning" role="alert">
+            <h1 className="text-center">Still not ready!</h1>
+        </div>
+    );
+}
+
+export default Queue;
--- resources/js/routes/RoutesPages.jsx
+++ resources/js/routes/RoutesPages.jsx
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 /* Pages */
 import NotFound from "@/pages/NotFound";
 import Cronjob from "@/pages/Cronjob";
+import Queue from "@/pages/Queue";
 
 function RoutesPages() {
     return (
         <Switch>
             <Route exact={true} path="/cronjobs" component={Cronjob} />
+            <Route exact={true} path="/cronjobs/queue" component={Queue} />
             <Route component={NotFound} />
         </Switch>
     );
